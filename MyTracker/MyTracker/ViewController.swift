a87e8635ba1b8f7367cf97db6cc6c8cc93a9d42f
Александр Лацинник <sasha.lats@icloud.com>
2025-08-15 17:22:45 +0300

    Initial commit

---
+++ MyTracker/MyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTracker
+//
+//  Created by sasha on 09.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
